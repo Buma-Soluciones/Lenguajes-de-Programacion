cf7efcbd23b97240b8ce212055ec9befd6580cc8
Juanito Becerra <jsbecerrab@unal.edu.co>
2019-09-25 11:01:21 -0500

    Token class

---
+++ LexicalAnalyzer/token.py
@@ -0,0 +1,22 @@
+class Token(object):
+    """ La estructura del token consiste en un tipo del token, su respectivo lexema y la columna y fila
+        donde se encuentra, ademas si el Token es una palabra reservada o simbolo especial, como su lexema es
+        igual, este no se representa en la estructura
+    """
+    def __init__(self, type, lex, row,col):
+        self.type = type
+        self.lex = lex
+        self.row = row
+        self.col = col
+
+    def __str__(self):
+        if(self.lex != ""):
+            return '<%s, %s, %s, %s>' % (self.type, self.lex, self.row, self.col)
+        else:
+            return '<%s, %s, %s>' % (self.type, self.row, self.col)    
+    
+
+token = Token("tk_cadena", "pene", 2,1)
+reserve_word = Token("if","",39,50)
+print(reserve_word)
+print(token)
