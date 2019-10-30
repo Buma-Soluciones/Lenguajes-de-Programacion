d7d86152be822e92bd9f54d12bc4c606fc0a6d5a
Juan Sebastian Peña <jspenaq@unal.edu.co>
2019-10-29 21:15:32 -0500

    :books: help message

--- Analyzer/main.py
+++ Analyzer/main.py
@@ -4,8 +4,26 @@
 """Funcion Main del analizador Lexico, es necesario tener los casos de prueba guardados
    en la carpeta entradas y estos casos se pasan como parametros por consola
 """
+import sys
 
-from syntaticAnalyzer import analyzeCode
+def help_msg():
+   print(
+            "Usage: main.py [OPTIONS] [FILE]\n"
+            "Syntatic analyzer and lexer analyzer code\n\n"
+            "OPTIONS:\n"
+            "--help: Print this message\n"
+            'FILE: The name of the code to analyze\n'
+        )
+
+def main():
+   arguments = sys.argv
+   if len(arguments) <= 1:
+      return help_msg()
+   if arguments[1] == "--help":
+      return help_msg()
+   else:
+      from syntaticAnalyzer import analyzeCode
+      analyzeCode()
 
 if __name__ == '__main__':
-    analyzeCode()
+   main()
